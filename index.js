b452a8eaaa32b9817b96af5e94ea5398c7de9c07
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-02-15 18:05:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const {add, list,name}=require('./item');
+console.log(add(12,23))
+console.log(list)
+console.log(name)
---
+++ item.js
@@ -0,0 +1,16 @@
+const add=(a,b)=>{
+    return a+b
+}
+const list=[
+    {
+        name:'dhiraj',
+        age:20
+    },
+    {
+        name:'neeraj',
+        age:25,
+    }
+
+]
+const name='dhiraj kumar tiwari'
+  module.exports={add,list,name};
